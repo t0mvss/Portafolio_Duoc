46c8b97f3a898bcec0146a338ca1c261d8addb22
t0mvss <to.konig@alumnos.duoc.cl>
2022-10-18 14:40:12 -0300

    Commit Pedido Detalle
    Se agrega segundo filtro de usuario, aún  falta que actualice al momento de enviar pedidos a mesa

--- Portafolio_SigloXXI/src/Capa_Vista/Vista_Pedidos_Pendientes.java
+++ Portafolio_SigloXXI/src/Capa_Vista/Vista_Pedidos_Pendientes.java
@@ -18,6 +18,7 @@ import javax.swing.JOptionPane;
 import javax.swing.RowFilter;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableRowSorter;
+import Capa_Modelo.TextPrompt;
 
 /**
  *
@@ -32,6 +33,8 @@ public class Vista_Pedidos_Pendientes extends javax.swing.JFrame {
     public Vista_Pedidos_Pendientes() {
         initComponents();
         mostrarTabla();
+        TextPrompt placeholder = new TextPrompt("ESTADO", txtFiltro1);
+        TextPrompt placeholder1 = new TextPrompt("ID USUARIO", txtFiltro);
     }
 
     public void mostrarTabla(){
@@ -65,12 +68,20 @@ public class Vista_Pedidos_Pendientes extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
         jScrollPane1 = new javax.swing.JScrollPane();
         tblPedidos = new javax.swing.JTable();
         btnAtras = new javax.swing.JButton();
         txtFiltro = new javax.swing.JTextField();
         btnEnviarPedido = new javax.swing.JButton();
         btnRefresh = new javax.swing.JButton();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        txtFiltro1 = new javax.swing.JTextPane();
+
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane3.setViewportView(jTextArea1);
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -114,6 +125,13 @@ public class Vista_Pedidos_Pendientes extends javax.swing.JFrame {
             }
         });
 
+        txtFiltro1.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtFiltro1KeyTyped(evt);
+            }
+        });
+        jScrollPane2.setViewportView(txtFiltro1);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -127,20 +145,22 @@ public class Vista_Pedidos_Pendientes extends javax.swing.JFrame {
                 .addComponent(btnAtras)
                 .addGap(33, 33, 33))
             .addGroup(layout.createSequentialGroup()
+                .addGap(80, 80, 80)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(txtFiltro, javax.swing.GroupLayout.PREFERRED_SIZE, 147, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                        .addGap(80, 80, 80)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 379, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(40, 40, 40)
+                        .addComponent(txtFiltro, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 379, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(144, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(20, 20, 20)
-                .addComponent(txtFiltro, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(txtFiltro, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 28, Short.MAX_VALUE)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 247, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
@@ -162,13 +182,11 @@ public class Vista_Pedidos_Pendientes extends javax.swing.JFrame {
 
     private void txtFiltroKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtFiltroKeyTyped
         
-        
-        
         txtFiltro.addKeyListener(new KeyAdapter() {
             @Override
             public void keyReleased(KeyEvent e) {
                 
-             trs.setRowFilter(RowFilter.regexFilter(txtFiltro.getText(), 3));
+             trs.setRowFilter(RowFilter.regexFilter(txtFiltro.getText(), 2));
                 
             }
             
@@ -180,8 +198,11 @@ public class Vista_Pedidos_Pendientes extends javax.swing.JFrame {
     private void btnEnviarPedidoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEnviarPedidoActionPerformed
         int i = JOptionPane.showConfirmDialog(this,"¿El pedido está listo para ser enviado?","Mensajes", JOptionPane.INFORMATION_MESSAGE);
         if (i == 0){
+            int fila = tblPedidos.getSelectedRow();
+            String id_usuario = tblPedidos.getValueAt(fila, 2).toString();
             try {
-                CallableStatement modificar = cc.prepareCall("{call actualizarPedidoDetalle()}");
+                CallableStatement modificar = cc.prepareCall("{call actualizarPedidoDetalle1(?)}");
+                modificar.setString(1, id_usuario);
                 modificar.execute();
                 JOptionPane.showMessageDialog(this,"¡Pedido Enviado con Éxito!","Mensajes", JOptionPane.INFORMATION_MESSAGE);
                 modelo.fireTableDataChanged();
@@ -196,14 +217,31 @@ public class Vista_Pedidos_Pendientes extends javax.swing.JFrame {
         
     }//GEN-LAST:event_btnRefreshActionPerformed
 
-    
+    private void txtFiltro1KeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtFiltro1KeyTyped
+       
+        txtFiltro1.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e) {
+                
+             trs.setRowFilter(RowFilter.regexFilter(txtFiltro1.getText(), 3));
+                
+            }
+            
+        });
+        trs = new TableRowSorter(modelo);
+        tblPedidos.setRowSorter(trs);
+    }//GEN-LAST:event_txtFiltro1KeyTyped
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnAtras;
     private javax.swing.JButton btnEnviarPedido;
     private javax.swing.JButton btnRefresh;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JTextArea jTextArea1;
     private javax.swing.JTable tblPedidos;
     private javax.swing.JTextField txtFiltro;
+    private javax.swing.JTextPane txtFiltro1;
     // End of variables declaration//GEN-END:variables
 }
